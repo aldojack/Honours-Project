f99787acf0e524d78105df9b736ed1c31f9fec75
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-10 18:31:50 +0000

    Refactored the function button and now takes in click handler prop

--- react/src/components/FunctionButton.jsx
+++ react/src/components/FunctionButton.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-export default function FunctionButton({BtnName}) {
+export default function FunctionButton({BtnName, clickHandler}) {
+
   return (
-    <button type="button" className="max-w-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{BtnName}</button>
+    <button 
+      type="button"
+      onClick={clickHandler}
+      className="max-w-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+      {BtnName}
+    </button>
 
   )
 }
