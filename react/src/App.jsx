29e89afc3f363a6001e13c05b474b2c94db92f42
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-10 18:29:01 +0000

    App component now holds data state and using useEffect to update local storage

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Favourites from './pages/Favourites'
 import Home from './pages/Home'
+import {buildData} from './data/buildData'
 
 function App() {
+
+  const [data, setData] = useState(() => {
+    const initialValue = localStorage.getItem('items')
+    
+    return initialValue !== null ? JSON.parse(initialValue) : []
+  });
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(data))
+  },[data]);
+
 
   return (
 
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home buildData={buildData} data={data} setData={setData}/>}/>
       <Route path="/favourites" element={<Favourites/>}/>
     </Routes>
   </BrowserRouter>
