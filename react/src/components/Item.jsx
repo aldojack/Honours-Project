16ef98708de7e644ffd53b0b39ccb2fd2d4a92d1
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-15 16:56:41 +0000

    Removed redundant react imports

--- react/src/components/Item.jsx
+++ react/src/components/Item.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 export default function Item({item}) {
   return (
-    <div className='item flex dark:bg-gray-200 bg-gray-800 p-4'>
-    <div className='item-data text-start'>
+    <div className='item flex dark:bg-gray-200 bg-gray-100 p-4'>
+    <div className='item-data text-start text-black'>
       <div className='item-category flex space-x-2'>
           <p className='bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded p-1 text-sm'>{item.category}</p>
       </div>
@@ -11,6 +11,7 @@ export default function Item({item}) {
         <p className='recipe-name'>Recipe: {item.name}</p>
         <p className='recipe-preptime'>Prep Time: {item.prepTime === 1 ? `${item.prepTime} minute` : `${item.prepTime} minutes`}</p>
         <p className='recipe-preptime'>Cook Time: {item.cookTime === 1 ? `${item.cookTime} minute` : `${item.cookTime} minutes`}</p>
+        {item.isFav && <p>❤</p>}
     </div>
 
     
--- react/src/components/ItemsContainer.jsx
+++ react/src/components/ItemsContainer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 import Item from './Item'
 export default function ItemsContainer({data}) {
   const items = data ? data.map((item) => <Item item={item} key={item.id}/>) : null
   return (
-    <div className='items-container bg-gray-200 dark:bg-gray-800 text-white dark:text-black w-96 mx-auto mt-12 p-4 space-y-4'>
+    <div className='items-container bg-gray-800 dark:bg-gray-800 text-white dark:text-black w-96 mx-auto mt-12 p-4 space-y-4'>
       {items || <p className='text-white'>No items to display</p>}
   </div>
   )
