9bb815b4e7c63f80aaafa463d1b611e6d96874b7
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-12-16 08:12:51 +0000

    Render no items to display if no items in the data set or local storage

--- react/src/components/ItemsContainer.jsx
+++ react/src/components/ItemsContainer.jsx
@@ -1,7 +1,9 @@
 
 import Item from './Item'
-export default function ItemsContainer({data}) {
-  const items = data ? data.map((item) => <Item item={item} key={item.id}/>) : null
+export default function ItemsContainer({data, setData}) {
+
+  const items = data.length ? data.map((item) => <Item item={item} key={item.id} setData={setData}/>) : null
+
   return (
     <div className='items-container bg-gray-800 dark:bg-gray-800 text-white dark:text-black w-96 mx-auto mt-12 p-4 space-y-4'>
       {items || <p className='text-white'>No items to display</p>}
