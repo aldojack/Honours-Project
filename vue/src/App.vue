986db2ba39efe1dc116647634e5198d6b00122f5
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-01-10 10:50:11 +0000

    Refactored and created HomeView page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,31 +1,9 @@
 <script setup>
-import Nav from '../src/components/Nav.vue'
-import Hero from '../src/components/Hero.vue'
-import FunctionButton from '../src/components/FunctionButton.vue'
-import ItemsContainer from './components/ItemContainer.vue';
-import Footer from './components/Footer.vue';
+import HomeView from './pages/HomeView.vue';
 
 
 </script>
 
 <template>
-  <Nav />
-  <div class='bg-white dark:bg-gray-900 flex-grow'>
-    <div class='w-2/3 mx-auto text-center'>
-      <Hero HeroText="The one stop shop to store recipes!" />
-      <main>
-        <h1 class="text-5xl font-extrabold dark:text-white">Recipe Manager</h1>
-        <div class='functions gap-4 grid grid-cols-2 md:grid-cols-3 mt-8 mx-auto w-fit'>
-          <FunctionButton />
-          <FunctionButton />
-          <FunctionButton />
-          <FunctionButton />
-          <FunctionButton />
-          <FunctionButton />
-        </div>
-        <ItemsContainer/>
-      </main>
-    </div>
-  </div>
-  <Footer />
+  <HomeView/>
 </template>
