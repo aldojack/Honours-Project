0fbf6f4a8450340d38ca487232c8ed66f5cef246
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-01-12 10:26:57 +0000

    Basic template for favourite page

---
+++ vue/src/pages/FavouritesView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import ItemsContainer from '@/components/ItemContainer.vue';
+
+
+</script>
+
+<template>
+    <div class='w-2/3 mx-auto text-center'>
+        <Hero HeroText="View all your favourited recipes here, if you have none view our Home for ideas" />
+        <main>
+            <h1 class="text-5xl font-extrabold dark:text-white">Favourite Recipes</h1>
+            <ItemsContainer />
+        </main>
+    </div>
+</template>
