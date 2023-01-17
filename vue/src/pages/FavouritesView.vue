0b735d3fbe5667e9439e017ef3f7443a3fd6ad05
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-01-17 12:52:41 +0000

    Favourites page now renders only favourited items

--- vue/src/pages/FavouritesView.vue
+++ vue/src/pages/FavouritesView.vue
@@ -1,7 +1,50 @@
 <script setup>
+import { reactive, watch, computed } from "vue";
 import Hero from '@/components/Hero.vue'
 import ItemsContainer from '@/components/ItemContainer.vue';
+import * as dataFunc from '@/data/buildData'
 
+
+const data = reactive({
+    items: JSON.parse(localStorage.getItem('items')) || []
+})
+
+const favourites = computed(() => {
+    return data.items.filter((item) => item.isFav)
+})
+
+watch(() => data.items, (newValue) => {
+    localStorage.setItem('items', JSON.stringify(newValue))
+}, { deep: true });
+
+const onEdit = (id) => {
+
+    const updatedItem = data.items.map(item => {
+        if (item.id === id) {
+            return dataFunc.updateOne(item)
+        }
+        return item
+    })
+    data.items = updatedItem;
+}
+
+const onDelete = (id) => {
+    const newArray = data.items.filter(item => {
+        return item.id !== id
+    })
+    data.items = newArray;
+}
+
+const onToggle = (id) => {
+    const toggledItem = data.items.map(item => {
+        if (item.id === id) {
+            return { ...item, isFav: !item.isFav }
+        }
+        return item
+    })
+    data.items = toggledItem;
+
+}
 
 </script>
 
@@ -10,7 +53,12 @@ import ItemsContainer from '@/components/ItemContainer.vue';
         <Hero HeroText="View all your favourited recipes here, if you have none view our Home for ideas" />
         <main>
             <h1 class="text-5xl font-extrabold dark:text-white">Favourite Recipes</h1>
-            <ItemsContainer />
+            <ItemsContainer
+              :items="favourites"
+              @delete="onDelete"
+              @edit="onEdit"
+              @toggle="onToggle"
+            />
         </main>
     </div>
 </template>
